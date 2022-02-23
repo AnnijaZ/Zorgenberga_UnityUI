f0d7297055d341218c673ddf1ea32661679ab604
Annija <annija.zorgenberga@gmail.com>
2022-02-23 15:48:54 +0200

    Iesakta apgerba vilksana

---
+++ Zorgenberga_UnityUI/Assets/Skripti/ApgerbaAizvilksana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ApgerbaAizvilksana : MonoBehaviour {
+	private RectTransform transformacijasLogs;
+	public Canvas kanva;
+
+	void Start () {
+		transformacijasLogs = GetComponent<RectTransform> ();
+	}
+
+	public void OnDrag(PointerEventData notikums){
+		transformacijasLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+	}
+}
