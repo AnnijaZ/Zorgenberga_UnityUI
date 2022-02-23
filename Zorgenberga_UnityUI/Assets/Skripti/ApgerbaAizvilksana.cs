0240775c24bc62a8421d0fb21b99214cf11277ee
Annija <annija.zorgenberga@gmail.com>
2022-02-23 17:00:41 +0200

    Nodrosinats audio un apraksts

--- Zorgenberga_UnityUI/Assets/Skripti/ApgerbaAizvilksana.cs
+++ Zorgenberga_UnityUI/Assets/Skripti/ApgerbaAizvilksana.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ApgerbaAizvilksana : MonoBehaviour {
+public class ApgerbaAizvilksana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
 	private RectTransform transformacijasLogs;
 	public Canvas kanva;
-
+	// Use this for initialization
 	void Start () {
 		transformacijasLogs = GetComponent<RectTransform> ();
 	}
 
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Kreisais kliksis uz obejkta!");
+	}
+
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Uzsākta objekta vilkšana!");
+	}
+
 	public void OnDrag(PointerEventData notikums){
 		transformacijasLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+	}
+
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log("Objekta vilkšana pārtraukta!");
 	}
 }
--- Zorgenberga_UnityUI/Assets/Skripti/TeluParadisana.cs
+++ Zorgenberga_UnityUI/Assets/Skripti/TeluParadisana.cs
@@ -7,11 +7,18 @@ public class TeluParadisana : MonoBehaviour {
 
 	public GameObject attelaTuretajs;
 	public Sprite[] attelumasivs;
+	public GameObject slaideris1;
+	public GameObject slaideris2;
 
 	public void izkritosais(int skaitlis){
 		if (skaitlis == 0)
 			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [0];
 		else if (skaitlis == 1)
 			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [1];
+	}
+
+	public void mainitLielumu(){
+		float pasreizejaVertiba = slaideris1.GetComponent<Slider> ().value;
+		attelaTuretajs.transform.localScale = new Vector2 (1F * pasreizejaVertiba, 1F * pasreizejaVertiba);
 	}
 }
