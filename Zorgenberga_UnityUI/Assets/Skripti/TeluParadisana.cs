bec620ee717596a6e2081c24525fa1d743e3a0fe
Annija <annija.zorgenberga@gmail.com>
2022-02-23 14:08:53 +0200

    Izkritosais saraksts

---
+++ Zorgenberga_UnityUI/Assets/Skripti/TeluParadisana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeluParadisana : MonoBehaviour {
+
+	public GameObject attelaTuretajs;
+	public Sprite[] attelumasivs;
+
+	public void izkritosais(int skaitlis){
+		if (skaitlis == 0)
+			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [0];
+		else if (skaitlis == 1)
+			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [1];
+	}
+}
