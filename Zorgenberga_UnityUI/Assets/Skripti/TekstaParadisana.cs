9d173037c2ddddb0b31e758e4a25be7915207b66
Annija <annija.zorgenberga@gmail.com>
2022-02-23 12:06:49 +0200

    Faila parvietosana

---
+++ Zorgenberga_UnityUI/Assets/Skripti/TekstaParadisana.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaParadisana : MonoBehaviour {
+
+	public string tekstsVards, tekstsGadi;
+
+	public GameObject ievadesLauksVards;
+
+	public GameObject ievadesLauksGadi;
+
+	public GameObject tekstaAttelosanasLauks;
+
+	public void darbibasArTekstu(){
+		tekstsVards = ievadesLauksVards.GetComponent<Text> ().text;
+		tekstsGadi = ievadesLauksGadi.GetComponent<Text> ().text;
+		tekstaAttelosanasLauks.GetComponent<Text>().text = "Tēla vārds ir "+tekstsVards+". Tam ir "+tekstsGadi+" gadi!"; 
+	}
+}
